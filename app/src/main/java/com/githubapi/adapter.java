1416db6db101ef0c59117096328dbea5fec815b4
A4G <dextorantony@yahoo.com>
2019-07-21 19:42:44 +0530

    Initial commit

---
+++ app/src/main/java/com/githubapi/adapter.java
@@ -0,0 +1,59 @@
+package com.githubapi;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aniket.githubapi.model.User;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import java.util.zip.Inflater;
+
+public class adapter extends RecyclerView.Adapter<adapter.holder> {
+    ArrayList<User> allusers;
+    private Context context;
+
+    public adapter(ArrayList<User> allusers, Context context){
+        this.allusers = allusers; this.context = context;
+
+    }
+    @NonNull
+    @Override
+    public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.view,parent,false);
+        return new holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull holder holder, int position) {
+        User new_user = allusers.get(position);
+        holder.t11.setText(new_user.getLogin());
+        holder.t2.setText(new_user.getType());
+        Picasso.get().load(new_user.getAvatarUrl()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return allusers.size();
+    }
+
+    class holder extends RecyclerView.ViewHolder{
+        TextView t11,t2;
+        ImageView image;
+        public holder(@NonNull View itemView) {
+            super(itemView);
+            t11 = itemView.findViewById(R.id.text1);
+            t2 = itemView.findViewById(R.id.text2);
+            image = itemView.findViewById(R.id.image);
+        }
+    }
+}
